496ab22e1aed67f69da6c18743b4d4b90ed70b62
Emad Amir <24242.imad@gmail.com>
2024-09-28 13:29:26 +0100

    added a basic structure to the practice typing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { randQuote } from '@ngneat/falso';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+
+randQuote();
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'practice-typing';
+  randomQuoteText = randQuote();
+  valueFromInput = '';
+  
+  onTypingHandler(event:Event)
+  {
+    this.valueFromInput = (event.target as HTMLInputElement).value;
+    console.log(this.valueFromInput);
+  }
+
+  checkIfTheTypedContentMatchesRandomText()
+  {
+    return this.valueFromInput === this.randomQuoteText;
+  }
+
+  compare(randomLetter: string, enteredLetter: string): string
+  {
+    if(this.valueFromInput == '' || !enteredLetter)
+    {
+      return "pending";
+    }
+    return randomLetter === enteredLetter ? "green" : "red"; 
+  }
 }
